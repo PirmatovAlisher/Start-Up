87ab347441de1f53aa179952d2dba308e98c9cb0
PirmatovAlisher <pirmatovalisher000@gmail.com>
2024-05-28 10:05:08 +0500

    Service Classes CRUD Operations (List-Add-Update-Delete)

--- ServiceLayer/Services/Abstract/IAboutService.cs
+++ ServiceLayer/Services/Abstract/IAboutService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityLayer.WebApplication.ViewModels.AboutVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace ServiceLayer.Services.Abstract
 {
     public interface IAboutService
     {
+        Task<List<AboutListVM>> GetAllListAsync();
+        Task AddAboutAsync(AboutAddVM request);
+        Task DeleteAboutAsync(int id);
+        Task<AboutUpdateVM> GetAboutById(int id);
+        Task UpdateAboutAsync(AboutUpdateVM request);
     }
 }
