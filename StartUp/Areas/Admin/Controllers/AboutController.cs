e8fc61c3df2931ede6496f24fec0eaf90001ea34
PirmatovAlisher <pirmatovalisher000@gmail.com>
2024-05-28 22:53:01 +0500

    About Controller - CRUD operations

---
+++ StartUp/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,61 @@
+﻿using EntityLayer.WebApplication.ViewModels.AboutVM;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Services.Abstract;
+
+namespace StartUp.Areas.Admin.Controllers
+{
+	public class AboutController : Controller
+	{
+		private readonly IAboutService _aboutService;
+
+		public AboutController(IAboutService aboutService)
+		{
+			_aboutService = aboutService;
+		}
+
+
+		public async Task<IActionResult> GetAboutList()
+		{
+			var aboutList = await _aboutService.GetAllListAsync();
+
+			return View(aboutList);
+		}
+
+
+		[HttpGet]
+		public IActionResult AddAbout()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> AddAbout(AboutAddVM request)
+		{
+			await _aboutService.AddAboutAsync(request);
+			return RedirectToAction("GetAboutList", "AboutController", new { Area = ("Admin") });
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> UpdateAbout(int id)
+		{
+			var about = await _aboutService.GetAboutById(id);
+
+			return View(about);
+		}
+		
+		[HttpPost]
+		public async Task<IActionResult> UpdateAbout(AboutUpdateVM request)
+		{
+			await _aboutService.UpdateAboutAsync(request);
+			return RedirectToAction("GetAboutList", "AboutController", new { Area = ("Admin") });
+		}
+
+
+		public async Task<IActionResult> DeleteAbout(int id)
+		{
+			await _aboutService.DeleteAboutAsync(id);
+			return RedirectToAction("GetAboutList", "AboutController", new { Area = ("Admin") });
+		}
+	}
+}
