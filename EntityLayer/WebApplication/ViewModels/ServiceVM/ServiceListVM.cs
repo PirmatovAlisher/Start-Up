72ddb17c038da133ec213096194b71a93151d556
PirmatovAlisher <pirmatovalisher000@gmail.com>
2024-06-03 19:41:45 +0500

    Service Controller CRUD Operations & cshtml Setup

--- EntityLayer/WebApplication/ViewModels/ServiceVM/ServiceListVM.cs
+++ EntityLayer/WebApplication/ViewModels/ServiceVM/ServiceListVM.cs
@@ -17,8 +17,8 @@ namespace EntityLayer.WebApplication.ViewModels.ServiceVM
 
         public string Name { get; set; } = string.Empty;
 
-        public string Description { get; set; } = string.Empty;
+        //public string Description { get; set; } = string.Empty;
 
-        public string Icon { get; set; } = string.Empty;
+        //public string Icon { get; set; } = string.Empty;
     }
 }
