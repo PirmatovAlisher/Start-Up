2cacfccc4177e7b49448c12dd3358d5ff8200b13
PirmatovAlisher <pirmatovalisher000@gmail.com>
2024-06-13 11:20:34 +0500

    identity Library - Advanced User Management Service - User Edit Preparation

---
+++ ServiceLayer/FluentValidation/Identity/UserEditValidation.cs
@@ -0,0 +1,34 @@
+﻿using EntityLayer.Identity.ViewModels;
+using FluentValidation;
+using ServiceLayer.Messages.Identity;
+using ServiceLayer.Messages.WebApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.FluentValidation.Identity
+{
+	public class UserEditValidation : AbstractValidator<UserEditVM>
+	{
+		public UserEditValidation()
+		{
+			RuleFor(x => x.UserName).
+				NotNull().WithMessage(ValidationMessages.NulEmptyMessage("User Name")).
+				NotEmpty().WithMessage(ValidationMessages.NulEmptyMessage("User Name"));
+
+			RuleFor(x => x.Email).
+				NotNull().WithMessage(ValidationMessages.NulEmptyMessage("Email")).
+				NotNull().WithMessage(ValidationMessages.NulEmptyMessage("Email")).
+				EmailAddress().WithMessage(IdentityMessages.CheckEmailAddress());
+
+			RuleFor(x => x.Password).
+				NotNull().WithMessage(ValidationMessages.NulEmptyMessage("Password")).
+				NotEmpty().WithMessage(ValidationMessages.NulEmptyMessage("Password"));
+
+			RuleFor(x => x.ConfirmNewPassword).
+				Equal(c => c.NewPassword).WithMessage(IdentityMessages.ComparePassword());
+		}
+	}
+}
---
+++ StartUp/Areas/User/Controllers/AuthenticationUserController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using EntityLayer.Identity.Entities;
+using EntityLayer.Identity.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StartUp.Areas.User.Controllers
+{
+	[Authorize]
+	[Area("User")]
+	public class AuthenticationUserController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly IMapper _mapper;
+
+		public AuthenticationUserController(UserManager<AppUser> userManager, IMapper mapper)
+		{
+			_userManager = userManager;
+			_mapper = mapper;
+		}
+
+
+		[HttpGet]
+		public async Task<IActionResult> UserEdit()
+		{
+			var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
+
+			var userEditVM = _mapper.Map<UserEditVM>(user);
+
+			return View(userEditVM);
+
+		}
+	}
+}
