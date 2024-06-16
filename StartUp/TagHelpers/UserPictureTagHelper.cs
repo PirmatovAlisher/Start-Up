1a1e3fa0eed6ee8b009e4c389cb3f1d915b100cf
PirmatovAlisher <pirmatovalisher000@gmail.com>
2024-06-16 23:40:11 +0500

    Tag Helper for Picture Implementation

---
+++ StartUp/TagHelpers/UserPictureTagHelper.cs
@@ -0,0 +1,38 @@
+﻿using EntityLayer.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StartUp.TagHelpers
+{
+	public class UserPictureTagHelper : TagHelper
+	{
+		private string FileName { get; set; } = null!;
+
+		private readonly SignInManager<AppUser> _signInManager;
+		private readonly UserManager<AppUser> _userManager;
+
+		public UserPictureTagHelper(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+		{
+			_signInManager = signInManager;
+			_userManager = userManager;
+		}
+
+		public override async Task<Task> ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "img";
+
+			var SignedInUserId= _signInManager.Context.User.Claims.First(x => x.Type.Contains("identifier")).Value;
+
+			var user = await _userManager.FindByIdAsync(SignedInUserId);
+
+			if (!string.IsNullOrEmpty(user!.FileName))
+			{
+				output.Attributes.SetAttribute("src", $"/images/{FileName}");
+				return base.ProcessAsync(context, output);
+			}
+
+			output.Attributes.SetAttribute("src", "/images/user/default.png");
+			return base.ProcessAsync(context, output);
+		}
+	}
+}
