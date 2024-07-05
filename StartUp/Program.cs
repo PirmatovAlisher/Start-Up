ec739b928bf83f2c783ab36612d617abbe25d116
PirmatovAlisher <pirmatovalisher000@gmail.com>
2024-07-05 09:59:38 +0500

    Final touches

--- StartUp/Program.cs
+++ StartUp/Program.cs
@@ -32,12 +32,12 @@ namespace StartUp
 			// Configure the HTTP request pipeline.
 			if (!app.Environment.IsDevelopment())
 			{
-				app.UseExceptionHandler("/Error/GeneralExceptions");
+				
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 
-			
+			app.UseExceptionHandler("/Error/GeneralExceptions");
 			//app.UseStatusCodePagesWithRedirects("/Error/PageNotFound");
 			app.UseStatusCodePagesWithReExecute("/Error/PageNotFound");
 
